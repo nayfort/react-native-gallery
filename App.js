90dca8b86a5b7160c32bf86e8507eea917b2beff
nayfort <mitrofanovvladislav2004@gmail.com>
2023-04-25 14:32:15 +0200

    Home

--- App.js
+++ App.js
@@ -1,67 +1,79 @@
-import React, {Component} from 'react';
-import { StyleSheet, View} from 'react-native';
-
-import {Provider} from 'react-redux';
-import {store} from './app/store/store'
-
-import {createStackNavigator, createAppContainer} from 'react-navigation';
-
-import GalleryScreen from './app/components/GalleryScreen'
-import DetailImage from './app/components/DetailImageScreen'
-import { NavigationContainer } from '@react-navigation/native';
-
-
-export default class App extends Component {
-    render() {
-        return (
-            <NavigationContainer>
-            <Provider store={store}>
-                <AppContainer>
-                    <View style={styles.container}>
-                        <GalleryScreen />
-                    </View>
-                </AppContainer>
-            </Provider>
-            </NavigationContainer>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-});
-
-const AppNavigator = createStackNavigator({
-    Gallery: {screen: GalleryScreen},
-    Image: {screen: DetailImage},
-});
-
-const AppContainer = createAppContainer(AppNavigator);
-//1
-
-
-//import { StatusBar } from 'expo-status-bar';
-//import { StyleSheet, Text, View } from 'react-native';
+//import React, {Component} from 'react';
+//import { StyleSheet, View} from 'react-native';
 //
-//export default function App() {
-//  return (
-//    <View style={styles.container}>
-//      <Text>Open up App.js to start working on your app!</Text>
-//      <StatusBar style="auto" />
-//    </View>
-//  );
+//import {Provider} from 'react-redux';
+//import {store} from './app/store/store'
+//
+//import {createStackNavigator, createAppContainer} from 'react-navigation';
+//
+//import GalleryScreen from './app/components/GalleryScreen'
+//import DetailImage from './app/components/DetailImageScreen'
+//
+//
+//export default class App extends Component {
+//    render() {
+//        return (
+//            <Provider store={store}>
+//                <AppContainer>
+//                    <View style={styles.container}>
+//                        <GalleryScreen />
+//                    </View>
+//                </AppContainer>
+//            </Provider>
+//        );
+//    }
 //}
 //
 //const styles = StyleSheet.create({
-//  container: {
-//    flex: 1,
-//    backgroundColor: '#fff',
-//    alignItems: 'center',
-//    justifyContent: 'center',
-//  },
+//    container: {
+//        flex: 1,
+//        justifyContent: 'center',
+//        alignItems: 'center',
+//        backgroundColor: 'white',
+//    },
 //});
+//
+//const AppNavigator = createStackNavigator({
+//    Gallery: {screen: GalleryScreen},
+//    Image: {screen: DetailImage},
+//});
+//
+//const AppContainer = createAppContainer(AppNavigator);
+
+
+
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
